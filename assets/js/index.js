41388f5c300eebd1b0b79df360ae71686f820483
Rifat Hossain <smrifat1411@gmail.com>
2023-03-08 01:39:14 +0600

    data fetch check and fixed fetch data on every input

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,27 @@
-console.log("Environment test");
+const dataFetch = async (searchInput) => {
+  const response = await axios.get("https://www.omdbapi.com/", {
+    params: {
+      apikey: "f82024a5",
+      s: searchInput,
+      // i:"tt0375093"
+    },
+  });
+
+  console.log(response.data);
+};
+
+const searchBox = document.querySelector("#searchbox");
+
+// Function that look after when user type on searchbox
+let remainDelay;
+const onType = (e) => {
+  if (remainDelay) {
+    clearInterval(remainDelay);
+  }
+
+  remainDelay = setTimeout((e) => {
+    dataFetch(e.target.value);
+  }, 1000);
+};
+
+searchBox.addEventListener("input", onType);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+// const defaultTheme = require('tailwindcss/defaultTheme')
+
+// module.exports = {
+//   darkMode: 'class', //false, class, media
+//   content: ["*.{html,js}"],
+//   theme: {
+//     screens:{
+//       'xs': '350px',
+//       ...defaultTheme.screens,
+//     },
+//     extend: {
+//       colors: {
+       
+//       },
+//       fontFamily: {
+//         'montserrat': ['Montserrat','sans-serif'],
+//         'poppins': ['Poppins','sans-serif'],
+       
+//       },
+//       fontSize: {
+//         'xxs': '.65rem'
+//       },
+      
+//     }
+//   },
+//   plugins: [],
+// }
